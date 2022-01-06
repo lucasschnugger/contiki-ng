459ac589128e5b11d122eaa07a1288a29b42a8ff
lucasschnugger <lucasschnugger@gmail.com>
2022-01-06 09:32:57 +0100

    Saved CSV data scraped from logs to file.

--- examples/6tisch/simple-node/simulations/logs_extract_csv.py
+++ examples/6tisch/simple-node/simulations/logs_extract_csv.py
@@ -2,8 +2,10 @@ import os
 
 if os.path.isdir("/home/user/"):
     log_dir = "/home/user/contiki-ng/examples/6tisch/simple-node/simulations/logs/"
+    output_file = "/home/user/data.csv"
 elif os.path.isdir("/Users/Lucas/"):
     log_dir = "/Users/Lucas/Documents/Projects/contiki-ng-fork/examples/6tisch/simple-node/simulations/logs/"
+    output_file = "/Users/Lucas/Downloads/data.csv"
 
 logs = [f for f in os.listdir(log_dir) if os.path.isfile(f"{log_dir}{f}")]
 logs.sort()
@@ -17,4 +19,7 @@ for log in logs:
                 csv_lines.append(line)
 
 csv_lines_str = "".join(csv_lines)
-print(csv_lines_str)
+file = open(output_file, "w")
+file.write(csv_lines_str)
+file.close()
+# print(csv_lines_str)
